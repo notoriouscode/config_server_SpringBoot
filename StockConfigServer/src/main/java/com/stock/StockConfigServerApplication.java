e69d9c19bd6189cc9c07ab1d889a671ee66bc969
Satyam <eng.satyam89@gmail.com>
2022-06-17 17:03:46 +0530

    spring code for configuration server

---
+++ StockConfigServer/src/main/java/com/stock/StockConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class StockConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockConfigServerApplication.class, args);
+	}
+
+}
